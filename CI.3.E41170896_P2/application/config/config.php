263999be7fdea6cc8519869af25ffac2d199e5ba
SyavinaOctavia <44762108+SyavinaOctavia@users.noreply.github.com>
2019-03-21 15:13:53 +0700

    syav 4-8

--- CI.3.E41170896_P2/application/config/config.php
+++ CI.3.E41170896_P2/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/tokobuah';
+$config['base_url'] = 'http://localhost:8080/htdocs/CI.3.E41170896_P2';
 
 /*
 |--------------------------------------------------------------------------
