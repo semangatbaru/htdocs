1187f3bb8ba9770e3c6b867fafcf6df027ae8f14
Yusufandi99 <44702492+Yusufandi99@users.noreply.github.com>
2019-03-28 17:08:29 +0700

    p3 yusuf
    polije siipp

--- CI.3.E41170913_P1/application/config/config.php
+++ CI.3.E41170913_P1/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/khizam45/';
+$config['base_url'] = 'http://localhost/htdocs/CI.3.E41170913_P1/';
 
 /*
 |--------------------------------------------------------------------------
